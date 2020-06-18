af979df320efc21ac8eb5ffacc4ed7230b17a9d0
TsujiRyota <xx.ryota.xx.brh0422@i.softbank.jp>
2020-06-18 11:36:50 +0900

    あほしね

---
+++ resources/views/selectCharacter.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="/caseStudy/resources/views/css/selectCharacter.css">
+    <title>case study</title>
+</head>
+<body>
+<div id="container">
+    <div class="row characterList">
+        <div class="col-md-1 h-100"></div>
+        <div class="col-md-2 h-100 character"><p>研究姓</p></div>
+        <div class="col-md-2 h-100 character"><p>社会性</p></div>
+        <div class="col-md-2 h-100 character"><p>現実性</p></div>
+        <div class="col-md-2 h-100 character"><p>芸術性</p></div>
+        <div class="col-md-2 h-100 character"><p>社交性</p></div>
+        <div class="col-md-1 h-100"></div>
+    </div>
+</div>
+</body>
+</html>
